72640ae90173c4192c06fe37dc439809e65aacce
Yangtao Ge <geyangtao@yahoo.com>
2020-08-26 22:16:38 +0100

    feat: workable blank UI

--- shopSmallApp/src/app/app.component.tsx
+++ shopSmallApp/src/app/app.component.tsx
@@ -4,24 +4,18 @@ import { AppearanceProvider } from 'react-native-appearance';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { AppLoading } from './app-loading.component';
 import { appMappings, appThemes } from './app-theming';
 import { AppIconsPack } from './app-icons-pack';
 import { StatusBar } from '../components/status-bar.component';
 import { SplashImage } from '../components/splash-image.component';
-import { Mapping, Theme, Theming } from '../services/theme.service';
+import { Theming } from '../services/theme.service';
 import {AppNavigator} from "../navigation/app.navigation";
 
 
-const defaultConfig: { mapping: Mapping, theme: Theme } = {
-    mapping: 'eva',
-    theme: 'light',
-};
+const App = (): React.ReactElement => {
 
-const App = ({ mapping, theme }): React.ReactElement => {
-
-    const [mappingContext, currentMapping] = Theming.useMapping(appMappings, mapping);
-    const [themeContext, currentTheme] = Theming.useTheming(appThemes, mapping, theme);
+    const [mappingContext, currentMapping] = Theming.useMapping(appMappings, "eva");
+    const [themeContext, currentTheme] = Theming.useTheming(appThemes, "eva", "light");
 
     return (
         <React.Fragment>
@@ -42,17 +36,13 @@ const App = ({ mapping, theme }): React.ReactElement => {
     );
 };
 
-const Splash = ({ loading }): React.ReactElement => (
-    <SplashImage
-        loading={loading}
-        source={require('../assets/images/shop-small.png')}
-    />
-);
+// const Splash = ({ loading }): React.ReactElement => (
+//     <SplashImage
+//         loading={loading}
+//         source={require('../assets/images/shop-small.png')}
+//     />
+// );
 
 export default (): React.ReactElement => (
-    <AppLoading
-        initialConfig={defaultConfig}
-        placeholder={Splash}>
-        {props => <App {...props}/>}
-    </AppLoading>
+    <App/>
 );
