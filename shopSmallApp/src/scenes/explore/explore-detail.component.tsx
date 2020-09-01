7b890a9161c3f3b4c826671a54771dccf75a93ad
Yangtao Ge <geyangtao@yahoo.com>
2020-09-01 21:47:16 +0100

    add scrolled view

--- shopSmallApp/src/scenes/explore/explore-detail.component.tsx
+++ shopSmallApp/src/scenes/explore/explore-detail.component.tsx
@@ -1,34 +1,74 @@
-import {StyleSheet, Text} from "react-native";
-import { SafeAreaLayout } from '../../components/safe-area-layout.component';
-import React from "react";
-import {Icon, TopNavigation, TopNavigationAction} from "@ui-kitten/components";
+import React, {useRef} from 'react';
+import {
+    View,
+    Text,
+    Image,
+    StyleSheet,
+    Dimensions,
+    StatusBar,
+    Platform,
+} from 'react-native';
+import HeaderImageScrollView, {
+    TriggeringView,
+} from 'react-native-image-header-scroll-view';
 
-const CardItemDetailScreen = ({navigation, route}) => {
+import {SafeAreaLayout} from '../../components/safe-area-layout.component';
+import * as Animatable from 'react-native-animatable';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import {Icon} from "@ui-kitten/components";
+
+const MIN_HEIGHT = Platform.OS === 'ios' ? 90 : 55;
+const MAX_HEIGHT = 350;
+
+const CardItemDetailScreen = ({navigation, route}): React.ReactElement => {
 
     const data = route.params.markerData;
+    const navTitleView = useRef(null);
 
     const BackIcon = (props) => (
         <Icon name='arrow-ios-back' {...props} />
     );
 
 
-    const renderBackAction = (): React.ReactElement => (
-        <TopNavigationAction
-            icon={BackIcon}
-            onPress={navigation.goBack}
-        />
-    );
+    // const renderBackAction = (): React.ReactElement => (
+    //     <TopNavigationAction
+    //         icon={BackIcon}
+    //         onPress={navigation.goBack}
+    //     />
+    // );
 
-    return(
+    return (
         <SafeAreaLayout
             style={styles.safeArea}
             insets='top'
         >
-            <TopNavigation
-                title={data.title}
-                leftControl={renderBackAction()}
-            />
-            <Text>CarItemDetails</Text>
+            <StatusBar barStyle="light-content"/>
+            <HeaderImageScrollView
+                maxHeight={MAX_HEIGHT}
+                minHeight={MIN_HEIGHT}
+                maxOverlayOpacity={0.6}
+                minOverlayOpacity={0.3}
+                renderHeader={() => (
+                    <Image source={data.image} style={styles.image}/>
+                )}
+                renderForeground={() => (
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.imageTitle}>{data.title}</Text>
+                    </View>
+                )}
+                renderFixedForeground={() => (
+                    <Animatable.View style={styles.navTitleView} ref={navTitleView}>
+                        <Text style={styles.navTitle}>{data.title}</Text>
+                    </Animatable.View>
+                )}
+            >
+                <View style={{height: 1000}}>
+                    <TriggeringView onHide={() => console.log("text hidden")}>
+                        <Text>Scroll Me!</Text>
+                    </TriggeringView>
+                </View>
+            </HeaderImageScrollView>
         </SafeAreaLayout>
     );
 };
@@ -37,6 +77,77 @@ export default CardItemDetailScreen;
 
 const styles = StyleSheet.create({
     safeArea: {
-        flex:1,
+        flex: 1,
+    },
+    image: {
+        height: MAX_HEIGHT,
+        width: Dimensions.get('window').width,
+        alignSelf: 'stretch',
+        resizeMode: 'cover',
+    },
+    title: {
+        fontSize: 20,
+    },
+    name: {
+        fontWeight: 'bold',
+    },
+    section: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
+        backgroundColor: 'white',
+    },
+    sectionTitle: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    sectionContent: {
+        fontSize: 16,
+        textAlign: 'justify',
+    },
+    categories: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexWrap: 'wrap',
+    },
+    categoryContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#FF6347',
+        borderRadius: 20,
+        margin: 10,
+        padding: 10,
+        paddingHorizontal: 15,
+    },
+    category: {
+        fontSize: 14,
+        color: '#fff',
+        marginLeft: 10,
+    },
+    titleContainer: {
+        flex: 1,
+        alignSelf: 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    imageTitle: {
+        color: 'white',
+        backgroundColor: 'transparent',
+        fontSize: 24,
+    },
+    navTitleView: {
+        height: MIN_HEIGHT,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: Platform.OS === 'ios' ? 40 : 5,
+        opacity: 0,
+    },
+    navTitle: {
+        color: 'white',
+        fontSize: 18,
+        backgroundColor: 'transparent',
+    },
+    sectionLarge: {
+        minHeight: 300,
     },
 });
