70638ce46d245d9312d38465160499bb1065d63a
Yangtao Ge <geyangtao@yahoo.com>
2020-09-03 11:18:52 +0100

    feat: add layout

--- shopSmallApp/src/scenes/layouts/user.component.tsx
+++ shopSmallApp/src/scenes/layouts/user.component.tsx
@@ -1,16 +1,99 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import {Icon, TopNavigation, TopNavigationAction} from '@ui-kitten/components';
 import { SafeAreaLayout } from '../../components/safe-area-layout.component';
+import {View, SafeAreaView, StyleSheet} from 'react-native';
+import {
+    Avatar,
+    Title,
+    Caption,
+    Text,
+    TouchableRipple,
+} from 'react-native-paper';
+
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import {TopNavigation} from "@ui-kitten/components";
 
 export const UserScreen = (props): React.ReactElement => {
     return (
         <SafeAreaLayout
             style={styles.safeArea}
             insets='top'>
-            <TopNavigation
-                title='User Page'
-            />
+
+            <View style={styles.container}>
+            <View style={styles.userInfoSection}>
+                <View style={{flexDirection: 'row', marginTop: 15}}>
+                    <Avatar.Image
+                        source={{
+                            uri: 'https://api.adorable.io/avatars/80/yangtao.ge@adorable.io.png',
+                        }}
+                        size={80}
+                    />
+                    <View style={{marginLeft: 20}}>
+                        <Title style={[styles.title, {
+                            marginTop:15,
+                            marginBottom: 5,
+                        }]}>Yangtao Ge</Title>
+                        <Caption style={styles.caption}>@yangtao.ge</Caption>
+                    </View>
+                </View>
+            </View>
+
+            <View style={styles.userInfoSection}>
+                <View style={styles.row}>
+                    <Icon name="map-marker-radius" color="#777777" size={20}/>
+                    <Text style={{color:"#777777", marginLeft: 20}}>London, United Kindom</Text>
+                </View>
+                <View style={styles.row}>
+                    <Icon name="phone" color="#777777" size={20}/>
+                    <Text style={{color:"#777777", marginLeft: 20}}>+44-1234567890</Text>
+                </View>
+                <View style={styles.row}>
+                    <Icon name="email" color="#777777" size={20}/>
+                    <Text style={{color:"#777777", marginLeft: 20}}>yangtao.ge.17@ucl.ac.uk</Text>
+                </View>
+            </View>
+
+            <View style={styles.infoBoxWrapper}>
+                <View style={[styles.infoBox, {
+                    borderRightColor: '#dddddd',
+                    borderRightWidth: 1
+                }]}>
+                    <Title>£ 242.50</Title>
+                    <Caption>Total Saving</Caption>
+                </View>
+                <View style={styles.infoBox}>
+                    <Title>20</Title>
+                    <Caption>Shoppings</Caption>
+                </View>
+            </View>
+
+            <View style={styles.menuWrapper}>
+                <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Icon name="heart-outline" color="#FF6347" size={25}/>
+                        <Text style={styles.menuItemText}>Favorites</Text>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Icon name="credit-card" color="#FF6347" size={25}/>
+                        <Text style={styles.menuItemText}>History</Text>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Icon name="account-check-outline" color="#FF6347" size={25}/>
+                        <Text style={styles.menuItemText}>Support</Text>
+                    </View>
+                </TouchableRipple>
+                <TouchableRipple onPress={() => {}}>
+                    <View style={styles.menuItem}>
+                        <Icon name="cog-outline" color="#FF6347" size={25}/>
+                        <Text style={styles.menuItemText}>Settings</Text>
+                    </View>
+                </TouchableRipple>
+            </View>
+            </View>
+
         </SafeAreaLayout>
     );
 };
@@ -18,5 +101,53 @@ export const UserScreen = (props): React.ReactElement => {
 const styles = StyleSheet.create({
     safeArea: {
         flex: 1,
+    },
+    container: {
+        marginTop: 20,
+    },
+    userInfoSection: {
+        paddingHorizontal: 30,
+        marginBottom: 25,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    caption: {
+        fontSize: 14,
+        lineHeight: 14,
+        fontWeight: '500',
+    },
+    row: {
+        flexDirection: 'row',
+        marginBottom: 10,
+    },
+    infoBoxWrapper: {
+        borderBottomColor: '#dddddd',
+        borderBottomWidth: 1,
+        borderTopColor: '#dddddd',
+        borderTopWidth: 1,
+        flexDirection: 'row',
+        height: 100,
+    },
+    infoBox: {
+        width: '50%',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    menuWrapper: {
+        marginTop: 10,
+    },
+    menuItem: {
+        flexDirection: 'row',
+        paddingVertical: 15,
+        paddingHorizontal: 30,
+    },
+    menuItemText: {
+        color: '#777777',
+        marginLeft: 20,
+        fontWeight: '600',
+        fontSize: 16,
+        lineHeight: 26,
     },
 });
